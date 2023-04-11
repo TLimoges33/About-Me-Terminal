5207c477143e58c32a6c78a1576f0e7eaf9dcbf8
TLimoges33 <tylimo56@gmail.com>
2023-04-11 09:05:36 -0400

    clean up

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ const contact = document.querySelector('#contact')
 const aboutContent = document.querySelector('#about-content')
 const contactContent = document.querySelector('#contact-content')
 
-
-
 about.addEventListener('click', () => {
   const aboutBox = new WinBox({
     title: 'About Me',
